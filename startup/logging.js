8eab80e2ceb1359fe10532b7b6599302fabf7dcb
alperen <alperenzahid@gmail.com>
2019-12-17 13:55:19 +0300

    Uncaught and Unhandled Exceptions

--- startup/logging.js
+++ startup/logging.js
@@ -7,13 +7,22 @@ const winston = require("winston");
 require("express-async-errors");
 
 module.exports = function() {
+  //Express haricinde olusan hatalar icin.
+  // process.on("uncaughtException", ex => {
+  //   winston.error(ex.message);
+  //   process.exit(1);
+  // });
+  //Winstonun bu eklentisi yukaridaki isi yapiyor.
   winston.handleExceptions(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: "uncaughtExceptions.log" })
   );
 
+  //Yukaridaki kod reject edilen promise hatalarini yakalamaz.
   process.on("unhandledRejection", ex => {
-    throw ex;
+    // winston.error(ex.message);
+    // process.exit(1);
+    throw ex; //Bunu throw ederek unhandled exception'a cevirdim. Yukarisi yakalar bunu.
   });
 
   winston.add(winston.transports.File, { filename: "logfile.log" });
