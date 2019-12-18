4850bd49670de7fa3edb36630b27b927d4249777
alperen <alperenzahid@gmail.com>
2019-12-18 17:03:15 +0300

    Unit Testing

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,5 +1,8 @@
 const config = require("config");
 
+//Bu fonksiyon icin unit test yazmaya calissak express'in next, status, send gibi
+//metotlarini mock etmemiz gerekecek. Unit test sisecek bunun gibi fonksiyonlar icin
+//integration test daha mantikli
 module.exports = function(req, res, next) {
   // 401 Unauthorized - Valid JWT verirsen girebilirsin demek
   // 403 Forbidden - Valid JWT olsa bile giremezsin demek.
